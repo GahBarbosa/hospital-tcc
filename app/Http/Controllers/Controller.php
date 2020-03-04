ff7a53cfffc4dfe132c6152b1e2315c2afec9254
gabriel <gabriel.araujo@grumft.com>
2020-03-04 12:38:40 -0300

    a

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -11,8 +11,8 @@ class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
+    // public function __construct()
+    // {
+    //     $this->middleware('auth');
+    // }
 }
--- app/Http/Controllers/HospitalController.php
+++ app/Http/Controllers/HospitalController.php
@@ -36,23 +36,7 @@ class HospitalController extends Controller
      */
     public function store(Request $request)
     {  
-       $hospital = new Hospital();
-       $hospital->nome = $request->nome;
-       $hospital->descricao=$request->descricao;
-       $hospital->usuario=$request->usuario;
-       $hospital->senha=$request->senha;
-       $hospital->lat=$request->lat;
-       $hospital->lon=$request->lon;
-       $hospital->a_pos=$request->a_pos;
-       $hospital->a_neg=$request->a_neg;
-       $hospital->b_pos=$request->b_pos;
-       $hospital->b_neg=$request->b_neg;
-       $hospital->ab_pos=$request->ab_pos;
-       $hospital->ab_neg=$request->ab_neg;
-       $hospital->o_pos=$request->o_pos;
-       $hospital->o_neg=$request->o_neg;
-    
-       $hospital->save();
+        Hospital::create($request);
        return redirect()->route('hospital');
 }
     
@@ -66,7 +50,6 @@ class HospitalController extends Controller
      */
     public function update(Request $request, $id)
     {
-       
         $novoNome = $request->nome;
         $hospital = Hospital::find($request->id);
         $hospital->nome = $novoNome;
